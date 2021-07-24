e852157ae77253611bf69e365503892ede865e60
Gurunath Gandikota <gurunath.sharma@gmail.com>
2021-07-24 16:17:50 +0000

    dev1

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ print("this is my first commit ... this statement is added in sony branch")
 print("from main")
 print("from samsung")
 print("this is from dev1")
+
+for i in sfdfsa:
+    sadfsadfj\
+            asfsadf
+            sadf
+            asfd
+            asfdsdf
